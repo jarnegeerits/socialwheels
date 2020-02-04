6df41a476bbdcbf8bf65ec51e20914ece0345a61
jarnegeerits <jarne.ge@gmail.com>
2020-02-04 15:41:55 +0100

    first push, project en components generated + bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { DashComponent } from './dash/dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    UsersComponent,
+    LoginComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
